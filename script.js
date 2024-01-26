5a0262c975f16ab63a6da8fc6aaf1c70a5eff4e6
Callum Wong <callumwhitewong@gmail.com>
2024-01-27 02:21:11 +0800

    Added More Features
    1. Added Tab Component for Education and Skills
    2. Added Back to Top Button
    3. Added 'KL' favicon
    4. Added wider gap between img container and text container

--- script.js
+++ script.js
@@ -43,3 +43,32 @@ const writeLoop = async () => {
 };
 
 writeLoop();
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+const tabContents = document.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => {tabContent.classList.remove('active')})
+        tabs.forEach(tab => {tab.classList.remove('active')})
+        target.classList.add('active')
+        tab.classList.add('active')
+    })
+})
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("topBtn").style.display = "block";
+  } else {
+    document.getElementById("topBtn").style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
